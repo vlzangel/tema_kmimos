8b261e145080b57f5212e046042fbcd1f9c1ee1b
Angel Veloz <vlzangel91@gmail.com>
2017-03-19 22:04:18 -0430

    Actualizaciones del tema

--- wp-content/themes/vlz/header.php
+++ wp-content/themes/vlz/header.php
@@ -6,12 +6,21 @@
 		<meta charset="<?php bloginfo('charset'); ?>">
 		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 		<link rel='stylesheet' id='open-sans-css'  href='https://fonts.googleapis.com/css?family=Open+Sans%3A300italic%2C400italic%2C600italic%2C300%2C400%2C600&#038;subset=latin%2Clatin-ext&#038;ver=4.4.7' media='all' />
+		<script src="https://code.jquery.com/jquery-3.2.0.min.js"></script>
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<meta http-equiv="Expires" content="0" />
+		<meta http-equiv="Pragma" content="no-cache" />
+
 		<?php wp_head(); ?>
 	</head>
 	<body>
 
 		<?php 
 			get_template_part( 'recursos/vlz', 'styles' ); 
+			get_template_part( 'recursos/vlz', 'jss' ); 
+			global $wpdb;
 		?>
 	
         <header>
--- wp-content/themes/vlz/recursos/vlz-styles.php
+++ wp-content/themes/vlz/recursos/vlz-styles.php
@@ -2,11 +2,11 @@
 	
 	body{
 		margin: 0px;
-	    padding-top: 100px;
+	    padding-top: 78px;
 	}
 
 	header{
-	    position: absolute;
+	    position: fixed;
 	    top: 0px;
 	    left: 0px;
 	    width: 100%;
@@ -15,6 +15,51 @@
 	    box-sizing: border-box;
 	    padding: 0px;
 	    margin: 0px;
+        z-index: 9999999999999999999999;
+	}
+
+	footer{
+		background-image: url(<?php echo get_template_directory_uri()."/recursos/imgs/footerBg.png"; ?>);
+	    background-repeat: no-repeat;
+	    background-size: cover;
+	    height: 420px;
+	    padding-top: 150px !important;
+	    padding-bottom: 20px !important;
+	    font-family: 'Roboto', sans-serif !important;
+	    font-style: italic !important;
+        color: #FFF;
+	}
+
+	footer ul{
+		list-style: none;
+	}
+
+	footer a{
+		text-decoration: none;
+		color: #FFF;
+	}
+
+	footer h2{
+		font-size: 21px;
+		font-weight: 400;
+	}
+
+	.socialBtn {
+	    width: 57px;
+	    height: 46px;
+	    display: inline-block;
+	}
+
+	.facebookBtn {
+	    background-image: url(<?php echo get_template_directory_uri()."/recursos/imgs/facebookBtn.png"; ?>);
+	}
+
+	.twitterBtn {
+	    background-image: url(<?php echo get_template_directory_uri()."/recursos/imgs/twitterBtn.png"; ?>);
+	}
+
+	.instagramBtn {
+	    background-image: url(<?php echo get_template_directory_uri()."/recursos/imgs/instagramBtn.png"; ?>);
 	}
 
 	/* Menu */
@@ -41,7 +86,9 @@
     		font-size: 15px;
 		}
 		.contenedor_menu{
-			float: right;
+		    position: absolute;
+		    top: 0px;
+		    right: 0px;
 		}
 		#menu-main{
 		    list-style: none;
@@ -60,7 +107,7 @@
 			position: relative;		
 		}
 		#menu-main > li > a {
-			padding: 30px 10px;
+			padding: 29px 10px 30px;
 			display: inline-block;
     		color: #FFF;
     		transition-duration: 0.2s;
@@ -81,7 +128,7 @@
 		}
 
 		#menu-main > li.menu-item-has-children > a{
-			padding: 30px 25px 30px 10px;
+			padding: 29px 25px 30px 10px;
 		}
 
 		#menu-main > li.menu-item-has-children::after{
@@ -135,6 +182,7 @@
 		}
 
 		#site-navigation-user .contenedor_menu_externo{
+    		position: relative;
 		    max-width: 1150px;
 		    margin: 0px auto;
 		}
@@ -160,4 +208,123 @@
 		    background: rgba(255, 255, 255, 0.2);
 		}
 
+	/* Mapa */
+
+      	.vlz_contenedor_mapa{
+      		position: relative;
+      		height: 450px;
+      	}
+
+      	#map { 
+      		height: 450px;
+      		z-index: 1;
+      	}
+
+      	.vlz_filtros { 
+		    position: absolute;
+		    bottom: 0px;
+		    left: 0px;
+		    width: 100%;
+		    z-index: 100;
+		    background: rgba(0, 0, 0, 0.5);
+		    box-sizing: border-box;
+		    text-align: center;
+		    padding: 20px;
+      	}
+
+      	.vlz_input{
+  		    padding: 10px;
+		    border-radius: 5px;
+		    width: 15%;
+		    outline: none;
+      	}
+
+      	.vlz_date{
+      		padding: 7px;
+		    border-radius: 5px;
+		    width: 15%;
+		    font-size: 16px !important;
+		    border: 0;
+		    outline: none;
+      	}
+
+      	.vlz_input:focus,
+      	.vlz_boton:focus,
+      	.vlz_date:focus
+      	{
+      		box-shadow: 0px 0px 0px #000;
+      	}
+
+      	.vlz_boton{
+  		    padding: 10px;
+		    border-radius: 5px;
+		    width: 15%;
+		    background: #6cba9b;
+		    color: white;
+		    font-size: 16px !important;
+		    border: 0;
+		    cursor: pointer;
+		    outline: none;
+      	}
+
+    /* Resultados */
+
+		.vlz_contenedor_cuidador {
+		    position: relative;
+		    display: inline-block;
+		    width: 25%;
+		    box-sizing: border-box;
+		    padding: 10px;
+		}
+
+    	.vlz_contenedor_foto{
+		    height: 300px;
+		    background-size: cover;
+		    background-position: center;
+		    background-repeat: no-repeat;
+		    border: solid 1px #CCC;
+		    z-index: 1;
+		    filter: blur(1px);
+    		overflow: hidden;
+    	}
+
+    	.vlz_foto{
+			height: 280px;
+			width: calc( 100% - 60px );
+			background-size: contain;
+			background-position: center;
+			background-repeat: no-repeat;
+			position: absolute;
+			top: 20px;
+			z-index: 100;
+			left: 30px;
+    	}
+
+    	.vlz_nombre{
+			padding: 5px;
+			font-size: 16px;
+			font-family: Roboto;
+			border: solid 1px #CCC;
+			border-top: 0;
+			border-bottom: 0;
+		    text-transform: capitalize;
+    	}
+
+    	.vlz_precio {
+			padding: 5px;
+			font-size: 16px;
+			font-family: Roboto;
+			text-align: right;
+			border: solid 1px #CCC;
+			border-top: 0;
+		}
+
+		a.vlz_link {
+		    position: absolute;
+		    top: 0px;
+		    left: 0px;
+		    width: 100%;
+		    height: 100%;
+		    z-index: 100;
+		}
 </style>
